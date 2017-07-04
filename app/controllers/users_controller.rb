a2cf6d18f3ba92727c930479dd888969b531ddd1
TranTuanManh <manhit1706@gmail.com>
2017-07-04 16:19:35 +0700

    Updating, showing and deleting users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
   def new
     @user = User.new
   end
@@ -14,10 +18,34 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.paginate page: params[:page]
+  end
+
   def show
     return if @user = User.find_by(id: params[:id])
     flash[:danger] = t ".error"
     redirect_to root_path
+  end
+
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes user_params
+      flash[:success] = t ".profileupd"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = t ".delete"
+    redirect_to users_url
   end
 
   private
@@ -25,5 +53,21 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit :name, :sex, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t ".login"
+    redirect_to login_url
+  end
+
+  def correct_user
+    @user = User.find params[:id]
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_reader :remember_token
+
   enum sex: [:male, :female]
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -7,13 +9,44 @@ class User < ApplicationRecord
     presence: true,
     uniqueness: {case_sensitive: false},
     length: {maximum: 255}
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true, on: :create
   validates :name, presence: true, length: {maximum: 50}
   validates :sex, presence: true
 
   has_secure_password
 
   before_save :email_downcase
+
+  class << self
+    def digest string
+      cost =
+        if ActiveModel::SecurePassword.min_cost
+          BCrypt::Engine.MIN_COST
+        else
+          BCrypt::Engine.cost
+        end
+
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    @remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.blank?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
 
   private
   def email_downcase
