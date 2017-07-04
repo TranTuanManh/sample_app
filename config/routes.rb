ec1f1a107e959a5846396e23b8a34f21e53075d8
TranTuanManh <manhit1706@gmail.com>
2017-07-04 13:24:37 +0700

    Basic login chap 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   root "static_pages#home"
+
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
   get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
+
+  post "/login", to: "sessions#create"
   post "/signup", to: "users#create"
+
+  delete "/logout", to: "sessions#destroy"
+
   resources :users, only: [:show]
 end
