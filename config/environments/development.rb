a19a84cfa7678feb6f50bbb0eec03da660f387cc
TranTuanManh <manhit1706@gmail.com>
2017-07-07 10:41:16 +0700

    Password reset chap 12

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,19 +27,20 @@ Rails.application.configure do
   end
 
   # Don"t care if the mailer can"t send.
-    config.action_mailer.raise_delivery_errors = true
-   config.action_mailer.delivery_method = :smtp
-   host = "localhost:3000"
-   config.action_mailer.default_url_options = { host: host, protocol: "http" }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = "localhost:3000"
+  config.action_mailer.default_url_options = {host: host, protocol: "http"}
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    domain: "gmail.com",
+    port: 587,
+    user_name: ENV["GUSERNAME"],
+    password: ENV["GPASSWORD"],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
-   config.action_mailer.smtp_settings = {
-     address: "smtp.gmail.com",
-     domain: "gmail.com",
-     port: 587,
-     user_name: ENV["GUSERNAME"],
-     password: ENV["GPASSWORD"],
-     authentication: :plain
-   }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
